0f77e3ca86f3842fcdacc4bd6c148fbeb9a94f00
JeonMoo <rjm0710@naver.com>
2022-02-05 19:23:25 +0900

    Spring Boot WelcomePage Setting

---
+++ src/main/java/com/test/cont/Appstarter.java
@@ -0,0 +1,13 @@
+package com.test.cont;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Appstarter {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Appstarter.class, args);
+    }
+
+}
---
+++ src/main/java/com/test/cont/ServletInitializer.java
@@ -0,0 +1,9 @@
+package com.test.cont;
+
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+public class ServletInitializer extends SpringBootServletInitializer {
+
+
+
+}
